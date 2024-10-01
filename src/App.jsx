7e810d92c9e738addac4998edc7d56d13dd04cb3
LighTnos29 <udit.2012005@gmail.com>
2024-10-01 17:39:19 +0530

    page loading issue solved

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css'
 import AllRoutes from './routes/AllRoutes'
 import { BrowserRouter as Router } from 'react-router-dom';
 import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
-import { motion, } from "framer-motion";
 import Footer from './components/Footer';
 
 
